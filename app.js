58d94168ad01c111075aa599a280ea7a07072e57
Lilian <210286008+liliann19@users.noreply.github.com>
2025-10-14 10:39:01 -0700

    Updated files and added node

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    //res.send('Welcome to My Ice Cream Shop!');
+    res.sendFile(`${import.meta.dirname}/views/home.html`)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
